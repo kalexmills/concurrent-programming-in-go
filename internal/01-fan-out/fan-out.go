3d31e7168a6608844e83eb3d53731700834d309d
K. Alex Mills <k.alex.mills@gmail.com>
2023-08-18 12:02:21 -0500

    fan-out step 2

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -8,24 +8,36 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	n := 10
+	numWorkers := 10
 
-	for i := 0; i < n; i++ {
+	workQueue := make(chan int) // un
+
+	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go func(msgID int) {
-			defer wg.Done()
-			DoRPC(msgID)
+		go func(workerID int) { // worker goroutines
+			defer wg.Done() // -- 'happens before' line 33
+			// keep receiving messages as long as there are messages
+			// to send... until the channel is closed
+			for msg := range workQueue {
+				DoRPC(workerID, msg)
+			}
+			fmt.Println("worker", workerID, "ended") // (happens after close at line 32)
 		}(i)
-		// guarantee: goroutine will start
 	}
-	wg.Wait() // blocks the main goroutine until the counter is zero
+
+	for i := 0; i < 100; i++ {
+		// blocks until a receive operation happens on another goroutine
+		workQueue <- i // sending 100 messages through the channel (happens before line 22)
+	}
+	close(workQueue) // closes the channel (happens before line 24)
+	//wg.Wait()        // blocks the main goroutine until the counter is zero
 	fmt.Println("program ended")
 }
 
 // DoRPC fakes a remote procedure call.
-func DoRPC(msgID int) {
-	fmt.Println("sending message id:", msgID)
+func DoRPC(workerID int, msg int) {
+	fmt.Printf("sending message %d from worker %d\n", msg, workerID)
 	time.Sleep(100 * time.Millisecond) // fake RPC.
 	// blocking call -- stops the currently running goroutine.
-	fmt.Println("message", msgID, "was sent")
+	fmt.Println("worker", workerID, ": message", msg, "was sent")
 }
