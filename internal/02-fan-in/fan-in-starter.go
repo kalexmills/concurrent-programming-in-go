b84e3f9bf821cfcad10c35e39ce1346f8f6d2a51
K. Alex Mills <k.alex.mills@gmail.com>
2023-10-19 13:38:01 -0500

    fan-in solution and buffered channels

--- internal/02-fan-in/fan-in-starter.go
+++ internal/02-fan-in/fan-in-starter.go
@@ -1,20 +1,46 @@
 package main
 
-import "sync"
-
-// https://go.dev/play/p/0YBVXu1N2CR
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	ch := make(chan int)
-	var wg sync.WaitGroup
 
-	// TODO: start 10 'producer' goroutines
-	// Each producer should generate and send 10 integers to the consumer using fanInChan.
+	var producerWg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		producerWg.Add(1)
+		go func(id int) {
+			defer func() {
+				producerWg.Done()
+				if id == 0 {
+					producerWg.Wait()
+					close(ch)
+				}
+			}()
+			for i := 0; i < 10; i++ {
+				ch <- i
+			}
+			fmt.Printf("producer %d stopped\n", id)
+		}(i)
+	}
 
+	// Each producer should generate and send 10 integers to the consumer using ch.
+	var consumerWg sync.WaitGroup
+	consumerWg.Add(1)
+	go func() {
+		defer consumerWg.Done()
+		for i := range ch {
+			fmt.Printf("received %d\n", i)
+		}
+		fmt.Println("consumer stopped")
+	}()
 	// TODO: start 1 consumer goroutine
-	// The consumer should receive all of the integers from fanInChan and print them out.
+	// The consumer should receive all of the integers from ch and print them out.
 
 	// Challenge: Use wait-groups to ensure that every goroutine returns before the main() func stops.
 
-	wg.Wait()
+	consumerWg.Wait()
+	fmt.Println("main goroutine complete")
 }
--- internal/03-buffered-channels/buffered-channels.go
+++ internal/03-buffered-channels/buffered-channels.go
@@ -1,7 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var wg sync.WaitGroup
+	ch := NewBufferedChannel(4)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			ch.Send(i)
+			fmt.Println("sent value: ", i)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			val := ch.Receive()
+			fmt.Println("received value: ", val)
+		}
+	}()
+	wg.Wait()
+}
+
+//   h
+// [ _ _ _ _ _ ]
+//   t
+
+type BufferedChannel struct {
+	buffer []int
+	head   int  // head always points to the next empty slot, if there is one.
+	tail   int  // tail always points to the next item to be received (if there is one).
+	isFull bool // tells us if the buffer is full (vs. empty)
+
+	mut *sync.Mutex // guards all the variables in a BufferedChannel.
+
+	waitingOnNotFull  *sync.Cond
+	waitingOnNotEmpty *sync.Cond
+}
+
+func NewBufferedChannel(size int) *BufferedChannel {
+	mut := &sync.Mutex{}
+	return &BufferedChannel{
+		buffer:            make([]int, size),
+		mut:               mut,
+		waitingOnNotEmpty: sync.NewCond(mut),
+		waitingOnNotFull:  sync.NewCond(mut),
+	}
+}
+
+var ErrBufferFull = errors.New("buffer was full")
+var ErrBufferEmpty = errors.New("buffer was empty")
+
+func (ch *BufferedChannel) Send(value int) {
+	ch.mut.Lock()
+	defer ch.mut.Unlock()
+
+	for ch.isFull {
+		ch.waitingOnNotFull.Wait() // releases the lock; lets others do things... and suspends this goroutine
+		// we have the lock again!
+	}
+
+	ch.buffer[ch.head] = value
+	ch.head = (ch.head + 1) % len(ch.buffer)
+
+	if ch.head == ch.tail {
+		ch.isFull = true
+	}
+	ch.waitingOnNotEmpty.Signal()
+}
+
+func (ch *BufferedChannel) Receive() int {
+	ch.mut.Lock()
+	defer ch.mut.Unlock()
+
+	for ch.head == ch.tail && !ch.isFull {
+		ch.waitingOnNotEmpty.Wait()
+	}
+
+	value := ch.buffer[ch.tail]
+	ch.tail = (ch.tail + 1) % len(ch.buffer)
+
+	ch.isFull = false
+	ch.waitingOnNotFull.Signal()
+	return value
 }
