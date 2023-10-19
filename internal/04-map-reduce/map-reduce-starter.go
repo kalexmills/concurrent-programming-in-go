5f649fb385d355d6f0a874796ec5356f3bd9e3bc
K. Alex Mills <k.alex.mills@gmail.com>
2023-10-19 10:35:11 -0500

    reference for class on 10-19-2023

---
+++ internal/04-map-reduce/map-reduce-starter.go
@@ -0,0 +1,30 @@
+package main
+
+// play https://go.dev/play/p/0HQJ81CDwwB
+
+var lines = []string{
+	"lorem ipsum is simply dummy text of the printing and typesetting industry",
+	"lorem ipsum has been the industry's standard dummy text ever since the",
+	"when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"it has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged",
+	"it was popularised in the with the release of letraset sheets containing lorem ipsum passages and more recently with desktop publishing software like aldus pagemaker including versions of lorem ipsum",
+}
+
+func main() {
+	numMappers := 3
+	numReducers := 26 // max at 26
+
+	lineChan := make(chan string)
+	wordChannels := make([]chan string, numReducers)
+	for i := 0; i < numReducers; i++ {
+		wordChannels[i] = make(chan string)
+	}
+	countChannel := make(chan map[string]int)
+
+	// 1. pass each line into lineChan
+	// 2. start mappers which read from lineChan, split lines into words, and send via wordChannels to the appropriate
+	//    reducers (pick a hash function to assign).
+	// 3. start reducers which read from wordChannels, form a local wordcount, and send the results to countChannel.
+	//
+	// Make sure everything shuts down gracefully before the process ends.
+}
