863dc28d3b35a29dc44082186ec7315916fdb3b9
K. Alex Mills <k.alex.mills@gmail.com>
2023-10-19 11:59:47 -0500

    fan-out final form

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
 func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+	ctx.Done() // a special channel that's closed when the context is cancelled.
+
 	numMsgs := 100
 	numWorkers := 10
 	var wg sync.WaitGroup
@@ -17,21 +23,38 @@ func main() {
 		wg.Add(1)
 		go func(workerID int) {
 			defer wg.Done()
-			for msgID := range workQueue {
-				SlowRPCs(workerID, msgID)
-			} // only way to break from a channel range loop is when channel is closed
+			for {
+				select {
+				case msgID, ok := <-workQueue:
+					if !ok {
+						fmt.Printf("worker %d found channel closed\n", workerID)
+						return
+					}
+					SlowRPCs(ctx, workerID, msgID)
+				case <-ctx.Done():
+					fmt.Printf("worker %d cancelled\n", workerID)
+					return
+				}
+			}
 		}(i)
 	}
 
+sendLoop:
 	for msgID := 0; msgID < numMsgs; msgID++ {
-		workQueue <- msgID // send the ith message (could be a blocking operation)
+		select {
+		case workQueue <- msgID: // blocks
+			// run some code if this channel op happens first
+		case <-ctx.Done():
+			break sendLoop
+		}
 	}
 	close(workQueue) // it is always the senders responsibility to close the channel
 
 	wg.Wait()
 }
 
-func SlowRPCs(workerID, msgID int) {
+func SlowRPCs(ctx context.Context, workerID, msgID int) {
 	time.Sleep(500 * time.Millisecond) // fake latency.
 	fmt.Printf("worker #%d sending RPC with value %d\n", workerID, msgID)
+	// TODO: keep passing ctx down into the network library
 }
