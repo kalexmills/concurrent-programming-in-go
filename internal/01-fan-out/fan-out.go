7e74976d4ed7a42ea7aeb35ce01a136773f631b7
K. Alex Mills <k.alex.mills@gmail.com>
2024-01-05 12:22:38 -0600

    feat: waitgroup

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(msgID int) {
+			DoRPC(msgID)
+			wg.Done()
+		}(i)
+		// ask the runtime to start a new goroutine
+		// at some point in the future a new goroutine will start
+		// and run DoRPC.
+	}
+	// i = 10
+	// guaranteed that 10 goroutines will start sometime...
+	// wg count cannot become 0 until every goroutine has stopped.
+	wg.Wait()
+}
+
+func DoRPC(msgID int) {
+	time.Sleep(150 * time.Millisecond)
+	fmt.Printf("sending message %d\n", msgID)
 }
