59286835b0fb5ccf2167e43ca9efd29b77fea252
K. Alex Mills <k.alex.mills@gmail.com>
2024-01-05 15:58:38 -0600

    map-reduce solution

--- internal/03-buffered-channels/buffered-channels.go
+++ internal/03-buffered-channels/buffered-channels.go
@@ -1,9 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
 
 // Based on: https://www.youtube.com/watch?v=KBZlN0izeiY
 
 func main() {
-	fmt.Println("hello world")
+	ch := NewBufferedChannel(4)
+
+	n := 100
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < n; i++ {
+			ch.Send(i) // blocks until space is available
+			fmt.Printf("sent %d\n", i)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < n; i++ {
+			msg := ch.Receive() // blocks until a msg is available
+			fmt.Printf("received %d\n", msg)
+		}
+	}()
+
+	wg.Wait()
+}
+
+type BufferedChannel struct {
+	data   []int
+	head   int  // next empty space to write to (if one exists)
+	tail   int  // next full space to read from
+	isFull bool // true when buffer is full
+
+	mut           *sync.Mutex
+	untilNotFull  *sync.Cond
+	untilNotEmpty *sync.Cond
+}
+
+func NewBufferedChannel(size int) *BufferedChannel {
+	mut := new(sync.Mutex)
+	return &BufferedChannel{
+		data:          make([]int, size),
+		mut:           mut,
+		untilNotEmpty: sync.NewCond(mut),
+		untilNotFull:  sync.NewCond(mut),
+	}
+}
+
+var ErrFull = errors.New("channel is full")
+var ErrEmpty = errors.New("channel is empty")
+
+func (ch *BufferedChannel) Send(msg int) {
+	ch.mut.Lock()
+	defer ch.mut.Unlock() // always defer in case of panic
+
+	for ch.head == ch.tail && ch.isFull {
+		ch.untilNotFull.Wait()
+		// unlocks the lock put goroutine to sleep until someone wakes it up
+		// relocks the lock when you wake up.
+	}
+
+	ch.data[ch.head] = msg
+	ch.head = (ch.head + 1) % len(ch.data)
+
+	if ch.head == ch.tail {
+		ch.isFull = true
+	}
+	ch.untilNotEmpty.Signal()
+}
+
+func (ch *BufferedChannel) Receive() int {
+	ch.mut.Lock()
+	defer ch.mut.Unlock()
+
+	for ch.head == ch.tail && !ch.isFull {
+		ch.untilNotEmpty.Wait()
+		// unlocks the lock put goroutine to sleep until someone wakes it up
+		// relocks the lock when you wake up.
+	}
+
+	msg := ch.data[ch.tail]
+	ch.tail = (ch.tail + 1) % len(ch.data)
+
+	ch.isFull = false
+	ch.untilNotFull.Signal() // wake up one goroutine waiting on a not-full buffer
+
+	return msg
 }
--- internal/04-map-reduce/map-reduce.go
+++ internal/04-map-reduce/map-reduce.go
@@ -1,14 +1,100 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
+
 // https://go.dev/play/p/CU8lt4mIflo
 
 var lines = []string{
-	"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
 	"Lorem Ipsum has been the industry's standard dummy text ever since the",
-	"when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-	"It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+	"when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+	"It has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged It was popularised in the with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
 }
 
 func main() {
-	// TODO: process lines into wordcount map[string]int using the map-reduce algorithm we discuss.
+	linesChan := make(chan string)
+	wordsChan := make([]chan string, 26)
+	for i := 0; i < 26; i++ {
+		wordsChan[i] = make(chan string)
+	}
+	wordCountChan := make(chan map[string]int)
+	numMappers := 3
+	numReducers := 26
+
+	// start the producer to feed lines into the system
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, line := range lines {
+			linesChan <- strings.ToLower(line)
+		}
+		close(linesChan)
+		fmt.Println("producer complete")
+	}()
+
+	// start the mappers to split lines into words and send to the correct reducers
+	var wgMapper sync.WaitGroup
+	for i := 0; i < numMappers; i++ {
+		wgMapper.Add(1)
+		go func(id int) {
+			defer func() {
+				wgMapper.Done()
+				if id == 0 {
+					wgMapper.Wait()
+					for i := 0; i < len(wordsChan); i++ {
+						close(wordsChan[i])
+					}
+				}
+				fmt.Printf("mapper %d complete\n", id)
+			}()
+			for line := range linesChan {
+				tokens := strings.Split(line, " ")
+				for _, token := range tokens {
+					token = strings.TrimSpace(token)
+					idx := int(token[0] - 'a')
+					wordsChan[idx] <- token
+				}
+			}
+		}(i)
+	}
+
+	// start the reducers to obtain accurate wordcounts
+	var wgReducer sync.WaitGroup
+	for i := 0; i < numReducers; i++ {
+		wgReducer.Add(1)
+		go func(workerID int) {
+			defer func() {
+				wgReducer.Done()
+				if workerID == 0 {
+					wgReducer.Wait()
+					close(wordCountChan)
+				}
+				fmt.Printf("reducer %d complete\n", workerID)
+			}()
+
+			count := make(map[string]int)
+			for word := range wordsChan[workerID] {
+				count[word]++
+			}
+			wordCountChan <- count
+		}(i)
+	}
+
+	// start the consumer to receive and print out counts
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for counts := range wordCountChan {
+			fmt.Printf("counts received: %v\n", counts)
+		}
+		fmt.Println("consumer complete!")
+	}()
+
+	wg.Wait()
+	fmt.Println("main complete")
 }
