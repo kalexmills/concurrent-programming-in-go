7128830885617966b35b2cba35e29f791a3f090c
K. Alex Mills <k.alex.mills@gmail.com>
2024-05-29 11:43:42 -0500

    fan out pattern complete

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
 func main() {
+	ctx, done := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer done()
+	ch := make(chan int)
+	n := 10
+
 	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
+	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func() {
-			LongRunningRPC(i)
-			wg.Done()
+			defer wg.Done()
+			for {
+				select {
+				case data, ok := <-ch:
+					if !ok {
+						fmt.Printf("worker #%d stopped; channel closed\n", i)
+						return
+					}
+					LongRunningRPC(ctx, data)
+				case <-ctx.Done():
+					fmt.Printf("worker #%d stopped; deadline expired\n", i)
+					return
+				}
+			}
 		}()
 		// guarantee: at some point in future, LongRunningRPC will run in parallel.
 	}
+
+loop:
+	for msg := 0; msg < 100; msg++ {
+		select {
+		case ch <- msg:
+		case <-ctx.Done():
+			break loop
+		}
+	}
+
+	close(ch)
 	wg.Wait()
+	fmt.Println("ending main func")
 }
 
-func LongRunningRPC(data int) {
+func LongRunningRPC(ctx context.Context, data int) {
 	fmt.Printf("sending %d to server\n", data)
-	time.Sleep(150 * time.Millisecond)
+	time.Sleep(150 * time.Millisecond) // TODO: pass context to networking library
 }
