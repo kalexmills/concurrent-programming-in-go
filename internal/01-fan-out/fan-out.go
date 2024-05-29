2b9bc3691f671ae266a555bde953d4a51177c3b1
K. Alex Mills <k.alex.mills@gmail.com>
2024-05-29 11:12:50 -0500

    goroutines first step

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	fmt.Println("hello world")
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			LongRunningRPC(i)
+			wg.Done()
+		}()
+		// guarantee: at some point in future, LongRunningRPC will run in parallel.
+	}
+	wg.Wait()
+}
+
+func LongRunningRPC(data int) {
+	fmt.Printf("sending %d to server\n", data)
+	time.Sleep(150 * time.Millisecond)
 }
