a8b84b4329d0632c16f58e9bf2e4909e0b1814b2
K. Alex Mills <k.alex.mills@gmail.com>
2024-08-09 13:34:07 -0500

    04 map-reduce starter

--- internal/04-map-reduce/map-reduce.go
+++ internal/04-map-reduce/map-reduce.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // https://go.dev/play/p/CU8lt4mIfl
 
 var lines = []string{
@@ -10,5 +15,47 @@ var lines = []string{
 }
 
 func main() {
+	linesCh := make(chan string)
 
+	go func() {
+		for _, line := range lines {
+			linesCh <- line
+		}
+	}()
+
+	numMappers := 3
+	numReducers := 3
+	wordsChs := make([]chan string, numReducers)
+	for i := 0; i < numReducers; i++ {
+		wordsChs[i] = make(chan string)
+	}
+
+	for i := 0; i < numMappers; i++ {
+		go func() {
+			for line := range linesCh {
+				words := strings.Split(strings.ToLower(line), " ")
+				for _, word := range words {
+					key := int(word[0]-'a') % numReducers
+					wordsChs[key] <- word
+				}
+			}
+		}()
+	}
+
+	countCh := make(chan map[string]int)
+	for i := 0; i < numReducers; i++ {
+		localCount := make(map[string]int)
+		go func() {
+			for word := range wordsChs[i] {
+				localCount[word]++
+			}
+			countCh <- localCount
+		}()
+	}
+
+	go func() {
+		for count := range countCh {
+			fmt.Println("got count:", count)
+		}
+	}()
 }
