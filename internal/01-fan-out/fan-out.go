8c1e3dae64605c4b78da0e0d4c08fb12c3e756ab
K. Alex Mills <k.alex.mills@gmail.com>
2024-08-09 11:14:31 -0500

    01 first step

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			DoRPC(i)
+		}()
+	}
+	// at some point in future: go runtime will start 10 goroutines
+	//    running DoRPC
+	wg.Wait()
+}
+
+func DoRPC(data int) {
+	time.Sleep(time.Millisecond * 150)
+	fmt.Printf("doing an RPC w/ data: %d\n", data)
 }
