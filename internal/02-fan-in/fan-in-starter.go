ccac071d311a31daa51c474a08bc9f8e5249fb69
K. Alex Mills <k.alex.mills@gmail.com>
2024-08-09 12:38:06 -0500

    02 with 11 goroutines

--- internal/02-fan-in/fan-in-starter.go
+++ internal/02-fan-in/fan-in-starter.go
@@ -15,7 +15,13 @@ func main() {
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func() {
-			defer wg.Done()
+			defer func() {
+				wg.Done()
+				if i == 0 {
+					wg.Wait()
+					close(ch)
+				}
+			}()
 			for j := 0; j < 10; j++ {
 				ch <- i
 			}
@@ -23,7 +29,6 @@ func main() {
 		}()
 	}
 
-	// TODO: start 1 consumer goroutine
 	// The consumer should receive all of the integers from fanInChan and print them out.
 	var wgConsumer sync.WaitGroup
 	wgConsumer.Add(1)
@@ -35,14 +40,6 @@ func main() {
 		fmt.Println("consumer done!")
 	}()
 	// Challenge: Use wait-groups to ensure that every goroutine returns before the main() func stops.
-
-	wgConsumer.Add(1)
-	go func() {
-		defer wgConsumer.Done()
-		wg.Wait()
-		close(ch)
-		fmt.Println("closer done!")
-	}()
 
 	wgConsumer.Wait()
 	fmt.Println("main done!")
