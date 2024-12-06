755ffcbc60f4383d12d5d50d92ffe7e12036abb5
K. Alex Mills <k.alex.mills@gmail.com>
2024-12-06 11:51:54 -0600

    part one; step 3

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -1,29 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	ch := make(chan int) // TODO: unbuffered channels sync properties
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*250)
+	defer cancel()
 
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	// block until timeout has elapsed
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			// continue receiving data from channel until.... no more data.
-			for data := range ch {
-				DoRPC(data)
+			for {
+				select {
+				case data, ok := <-ch:
+					if !ok { // channel was closed
+						fmt.Printf("goroutine %d complete; channel closed\n", i)
+						return
+					}
+					DoRPC(data)
+				case <-ctx.Done():
+					fmt.Printf("goroutine %d complete; timeout\n", i)
+					return
+				}
 			}
-			fmt.Printf("go routine %d complete\n", i)
 		}()
 	}
 
+loop:
 	for i := 0; i < 100; i++ {
-		ch <- i
+		select {
+		case ch <- i:
+			// do nothing
+		case <-ctx.Done():
+			fmt.Println("timeout elapsed; sender")
+			break loop
+		}
 	}
 	close(ch) // signal that no further data is coming
 
