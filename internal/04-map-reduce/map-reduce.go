b2657630a94f4b1d485579661c56cad025fbe204
K. Alex Mills <k.alex.mills@gmail.com>
2024-12-06 13:43:18 -0600

    part 4 starter

--- internal/04-map-reduce/map-reduce.go
+++ internal/04-map-reduce/map-reduce.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 var lines = []string{
 	"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
 	"Lorem Ipsum has been the industry's standard dummy text ever since the",
@@ -8,5 +13,51 @@ var lines = []string{
 }
 
 func main() {
-	// TODO: implement map reduce
+	numMappers := 3
+	numReducers := 5
+	linesChan := make(chan string)
+	wordsChan := make([]chan string, numReducers)
+	countChan := make(chan map[string]int)
+	for i := 0; i < numReducers; i++ {
+		wordsChan[i] = make(chan string)
+	}
+
+	// start producer
+	go func() {
+		for _, line := range lines {
+			linesChan <- line
+		}
+	}()
+
+	// start mappers
+	for i := 0; i < numMappers; i++ {
+		go func() {
+			for line := range linesChan {
+				words := strings.Split(line, " ")
+				for _, word := range words {
+					lowered := strings.ToLower(words[i])
+					reducerID := int(lowered[0]-'a') % numReducers
+					wordsChan[reducerID] <- word
+				}
+			}
+		}()
+	}
+
+	// start reducers
+	for i := 0; i < numReducers; i++ {
+		go func() {
+			localCount := make(map[string]int)
+			for word := range wordsChan[i] {
+				localCount[word]++
+			}
+			countChan <- localCount
+		}()
+	}
+
+	// start consumer
+	go func() {
+		for count := range countChan {
+			fmt.Printf("received count: %v\n", count)
+		}
+	}()
 }
