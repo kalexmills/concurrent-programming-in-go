48e98d0beaf96396761e41d37d793a0d3c3b2a2e
K. Alex Mills <k.alex.mills@gmail.com>
2024-12-06 11:33:15 -0600

    part one; step two

--- internal/01-fan-out/fan-out.go
+++ internal/01-fan-out/fan-out.go
@@ -8,15 +8,27 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	for i := 0; i < 100; i++ {
+	ch := make(chan int) // TODO: unbuffered channels sync properties
+
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			DoRPC(i)
+			// continue receiving data from channel until.... no more data.
+			for data := range ch {
+				DoRPC(data)
+			}
+			fmt.Printf("go routine %d complete\n", i)
 		}()
-		// guarantee: at some point in future DoRPC will start (on another goroutine).
 	}
-	wg.Wait() // guaranteed: 10 goroutines will start later
+
+	for i := 0; i < 100; i++ {
+		ch <- i
+	}
+	close(ch) // signal that no further data is coming
+
+	wg.Wait()
+	fmt.Println("main thread complete")
 }
 
 func DoRPC(data int) {
